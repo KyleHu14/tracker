43d17c0dcfd7d56eb12e6824b85eded2b84642d6
Kyle Hu <lloydpearce4@gmail.com>
2025-01-12 17:03:57 -0800

    reset everything, starting from scratch again

--- src/components/signout-button.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { Button } from "./ui/button"
-import { signOut } from "@/app/actions"
-
-export default function SignOutButton() {
-    return <Button onClick={async () => signOut()}>Sign Out</Button>
-}
