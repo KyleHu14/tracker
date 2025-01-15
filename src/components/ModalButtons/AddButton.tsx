6cfc2a54707a9b7825a45a1536c10356387cb1df
Kyle Hu <lloydpearce4@gmail.com>
2025-01-14 23:02:28 -0800

    updated db schema, form is in progress

--- src/components/ModalButtons/AddButton.tsx
+++ src/components/ModalButtons/AddButton.tsx
@@ -3,15 +3,11 @@ import {
     Dialog,
     DialogContent,
     DialogDescription,
-    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import FieldContainer from "./FieldContainer"
-import FieldLabel from "./FieldLabel"
+import JobAppForm from "./JobAppForm"
 
 interface Props {
     className?: string
@@ -31,29 +27,7 @@ export default function AddButton({ className }: Props) {
                         below.
                     </DialogDescription>
                 </DialogHeader>
-                <div className="grid gap-4 py-4">
-                    <FieldContainer>
-                        <FieldLabel htmlFor="title">Title</FieldLabel>
-
-                        <Input
-                            id="title"
-                            defaultValue="Pedro Duarte"
-                            className="col-span-3"
-                        />
-                    </FieldContainer>
-
-                    <FieldContainer>
-                        <FieldLabel htmlFor="location">Location</FieldLabel>
-                        <Input
-                            id="location"
-                            defaultValue="Pedro Duarte"
-                            className="col-span-3"
-                        />
-                    </FieldContainer>
-                </div>
-                <DialogFooter>
-                    <Button type="submit">Create</Button>
-                </DialogFooter>
+                <JobAppForm />
             </DialogContent>
         </Dialog>
     )
---
+++ src/db/schema/job-application.ts
@@ -0,0 +1,26 @@
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core"
+
+import { user } from "./auth"
+
+export const job_application = pgTable("job_application", {
+    id: text("id").primaryKey(),
+    title: text("title").notNull(),
+    company: text("company").notNull(),
+    location: text("location").notNull(),
+    status: text("status", {
+        enum: [
+            "pending",
+            "interview",
+            "rejected",
+            "assessment",
+            "ghosted",
+            "offer",
+            "accepted",
+        ],
+    }).notNull(),
+    link: text("link").notNull(),
+    date: timestamp("date").notNull(),
+    userId: text("user_id")
+        .notNull()
+        .references(() => user.id),
+})
