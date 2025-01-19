29196173895e7e7b32778d2c6c98b2d12c3c9a49
Kyle Hu <lloydpearce4@gmail.com>
2025-01-18 18:43:48 -0800

    test

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,9 @@
 import { createAuthClient } from "better-auth/react"
+
+if (process.env.NEXT_PUBLIC_BASE_URL) {
+    throw Error("BASE URL DOES NOT EXIST")
+}
+
 export const authClient = createAuthClient({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000", // the base url of your auth server
 })
